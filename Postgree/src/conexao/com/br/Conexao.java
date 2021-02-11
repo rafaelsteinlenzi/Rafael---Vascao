ea8c8b86ab9b931fb2b8b2a1b0711317ffeb998f
rafaelsteinlenzi <69546566+rafaelsteinlenzi@users.noreply.github.com>
2021-02-10 21:26:09 -0300

    novos comandos com update e delete

--- Postgree/src/conexao/com/br/Conexao.java
+++ Postgree/src/conexao/com/br/Conexao.java
@@ -2,6 +2,7 @@
 package conexao.com.br;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -11,29 +12,38 @@ public class Conexao {
 	public static void main(String[] args) {
 		Connection conn = new ConnectionFactory().getConnection();
 		
-		String comando = "CREATE TABLE IF NOT EXISTS BENEFICIARIO ("
+		String comando = "CREATE TABLE IF NOT EXISTS CLIENTE ("
 				+ "id serial not null,"
 				+ "nome varchar(40) not null,"
-				+ "telefone varchar(15) not null);";
+				+ "email varchar(60) not null);";
 		
 		try {
 			Statement statement = conn.createStatement();
 			statement.execute(comando);
 			
-			String insert = "INSERT INTO BENEFICIARIO (nome, telefone) values ('Pelé', '4799796262')";
+			String insert = "INSERT INTO CLIENTE (nome, email) values ('biro-biro', 'biro@biro.com.br')";
 			statement.execute(insert);
 			
-			String insert2 = "INSERT INTO BENEFICIARIO (nome, telefone) values ('Teso', '479918563')";
+			String insert2 = "INSERT INTO CLIENTE (nome, email) values ('biro', 'biro@biro.com')";
 			statement.execute(insert2);
 			
-			String insert3 = "INSERT INTO BENEFICIARIO (nome, telefone) values ('Andre', '4733870172')";
-			statement.execute(insert3);
+			String delete = "DELETE FROM CLIENTE WHERE ID = 5";
+			statement.execute(delete);
 			
-			String insert4 = "INSERT INTO BENEFICIARIO (nome, telefone) values ('Andorinha', '479632689')";
-			statement.execute(insert4);
+			String update = "UPDATE cliente SET NOME = 'Kassioevascaino' where id = 4";
+			statement.execute(update);
 			
-			String insert5 = "INSERT INTO BENEFICIARIO (nome, telefone) values ('Thuane', '4799653215')";
-			statement.execute(insert5);
+			String update2 = "UPDATE cliente SET NOME = 'Kassioamaovasco' where id = 3";
+			statement.execute(update2);
+			
+			String select = "SELECT ID, NOME, EMAIL FROM CLIENTE";
+			ResultSet resultset = statement.executeQuery(select);
+			
+			while (resultset.next()) {
+				System.out.println("id: " + resultset.getInt("id"));
+				System.out.println("Nome " + resultset.getString("nome"));
+				System.out.println("Email: " + resultset.getString("email"));
+			}
 			
 			System.out.println("comando executado com sucesso!");
 			
