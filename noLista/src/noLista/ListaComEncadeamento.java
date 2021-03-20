06cac654f9e919a31d5dade36bdb80f4648c5434
rafaelsteinlenzi <69546566+rafaelsteinlenzi@users.noreply.github.com>
2021-03-19 21:35:42 -0300

    Estrutura Dados

---
+++ noLista/src/noLista/ListaComEncadeamento.java
@@ -0,0 +1,46 @@
+package noLista;
+
+public class ListaComEncadeamento {
+	private NoLista first;
+	private NoLista last;
+	private int counter;
+	
+	public ListaComEncadeamento() {
+		
+		this.first = null;
+		this.last  = null;
+		this.counter = 0;
+	}
+	
+	public void add(Integer obj) {
+		
+	}
+	
+	public void add(int counter, Integer last) {
+		
+	}
+	
+	public Integer (int counter) {
+		
+	}
+	
+	public boolean removeFirst(Integer indice) {
+		
+	}
+	
+	public void clear() {
+		first = null;
+		last  = null;
+		counter = 0;
+	}
+	
+	public int size() {
+		return counter;
+	}
+	
+	public boolean isEmpty() {
+		
+		return (counter == 0);
+		
+	}
+}
