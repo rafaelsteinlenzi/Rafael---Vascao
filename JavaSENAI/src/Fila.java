6e46af86ce0c491ef2b83366f6cf2efce203a3d0
rafaelsteinlenzi <69546566+rafaelsteinlenzi@users.noreply.github.com>
2021-04-09 21:38:13 -0300

    Aula Matheus

---
+++ JavaSENAI/src/Fila.java
@@ -0,0 +1,9 @@
+
+public interface Fila {
+
+	void insere(Object v) throws Exception;
+	Object retira() throws Exception;
+	boolean vazia();
+	void libera();
+	
+}
---
+++ JavaSENAI/src/FilaLista.java
@@ -0,0 +1,44 @@
+
+public abstract class FilaLista implements Fila {
+
+	public NoLista ini;
+	public NoLista fim;
+	
+	
+	public FilaLista(NoLista ini, NoLista fim) {
+		super();
+		this.ini = ini;
+		this.fim = fim;
+	}
+
+
+	@Override
+	public void insere(Object v) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public Object retira() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public boolean vazia() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+	@Override
+	public void libera() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+}
